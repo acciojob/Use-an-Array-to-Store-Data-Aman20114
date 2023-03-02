1d7898f80dfad2b556c45b9bfcf771b6ae46950f
Aman Tiwari <110708815+Aman20114@users.noreply.github.com>
2023-03-02 11:05:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,2 @@
-// var yourArray = ['apple', 10, true, 'banana', 25.5];
-// console.log(yourArray.length);
-let complexArray = [
-  [
-    {
-      one: 1,
-      two: 2
-    },
-    {
-      three: 3,
-      four: 4
-    }
-  ],
-  [
-    {
-      a: "a",
-      b: "b"
-    },
-    {
-      c: "c",
-      d: "d"
-    }
-  ]
-];
+let simpleArray = ['one', 2, 'three', true, false, undefined, null];
+console.log(simpleArray.length);
